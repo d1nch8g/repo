9513061014b9672c1231995978dda4db69e2d353
dancheg <dancheg@fmnx.su>
2023-03-18 17:07:15 +0700

    fixed build

--- services/httpservers/handlers.go
+++ services/httpservers/handlers.go
@@ -1,10 +1,13 @@
 package httpservers
 
 import (
-	"fmt"
-
 	"context"
+	"fmt"
 	"net/http"
+	"net/url"
+	"path"
+	"strings"
+	"time"
 
 	pb "dancheg97.ru/dancheg97/ctlpkg/gen/proto/v1"
 	"github.com/gorilla/mux"
@@ -21,6 +24,42 @@ type Params struct {
 	PkgsDir   string
 	WebDir    string
 	PacmanSvc pb.PacmanServiceServer
+}
+
+func WebForward(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
+	urlStr := "/web"
+	if u, err := url.Parse(urlStr); err == nil {
+		oldpath := r.URL.Path
+		if oldpath == "" {
+			oldpath = "/"
+		}
+		if u.Scheme == "" {
+			if urlStr == "" || urlStr[0] != '/' {
+				olddir, _ := path.Split(oldpath)
+				urlStr = olddir + urlStr
+			}
+
+			var query string
+			if i := strings.Index(urlStr, "?"); i != -1 {
+				urlStr, query = urlStr[:i], urlStr[i:]
+			}
+
+			trailing := strings.HasSuffix(urlStr, "/")
+			urlStr = path.Clean(urlStr)
+			if trailing && !strings.HasSuffix(urlStr, "/") {
+				urlStr += "/"
+			}
+			urlStr += query
+		}
+	}
+
+	w.Header().Set("Location", urlStr)
+	w.WriteHeader(200)
+
+	if r.Method == "GET" {
+		note := "<a href=\"" + urlStr + "\"> redirected </a>.\n"
+		fmt.Fprintln(w, note)
+	}
 }
 
 func RunHttpWrapper(params Params) error {
@@ -40,10 +79,22 @@ func RunHttpWrapper(params Params) error {
 	fs2 := http.FileServer(http.Dir(params.PkgsDir))
 	r.PathPrefix("/pkg/").Handler(http.StripPrefix("/pkg/", fs2))
 
+	rootPatt := runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{""}, ""))
+	svmux.Handle("GET", rootPatt, WebForward)
+
 	r.PathPrefix("/").Handler(svmux)
 
 	http.Handle("/", r)
 	logrus.Info("server running...")
 
-	return http.ListenAndServe(fmt.Sprintf(`:%d`, params.HttpPort), r)
+	server := http.Server{
+		Addr:              fmt.Sprintf(`:%d`, params.HttpPort),
+		Handler:           r,
+		ReadTimeout:       time.Minute,
+		ReadHeaderTimeout: time.Minute,
+		WriteTimeout:      time.Minute,
+		IdleTimeout:       time.Minute,
+	}
+
+	return server.ListenAndServe()
 }
